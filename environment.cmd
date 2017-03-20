762a79dd1e0b1795eb4738736993ddbac63a0b6a
Oleg <alegorium@gmail.com>
2017-03-20 15:05:35 +0300

    Run configure.cmd automagically

--- environment.cmd
+++ environment.cmd
@@ -6,4 +6,8 @@ set PATH=%PATH%;%TOOLS%;%TOOLS%\bin;%TOOLS%\i386;%TOOLS%\i386\bin;%CURRENT%
 set C_INCLUDE_PATH=
 set CPLUS_INCLUDE_PATH=
 set LIBRARY_PATH=
+set ROS_ARCH=i386
+if not exist "output-MinGW-i386" (
+    call "configure.cmd"
+)
 cd output-MinGW-i386
